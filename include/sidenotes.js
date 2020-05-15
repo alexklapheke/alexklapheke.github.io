53e0f6d650eddb1ee7d4f965fcf807d7a0671c45
Alex Klapheke <alexklapheke@gmail.com>
2020-05-14 21:23:45 -0400

    Restyle blog

--- include/sidenotes.js
+++ include/sidenotes.js
@@ -10,16 +10,16 @@ window.onload = function() {
 
 		// get height of last note
 		var num = parseInt(fn.id.replace(/fn/, ''));
-		var prev = document.getElementById("fn" + (num-2));
-		var prevbot = (num < 3) ? 0 : prev.getBoundingClientRect().bottom - bodytop;
+		var prev = document.getElementById("fn" + (num-1));
+		var prevbot = (num < 2) ? 0 : prev.getBoundingClientRect().bottom - bodytop;
 
-		// put even notes on left
-		if (!(num % 2)) fn.classList.add("leftnote");
+		// // put even notes on left
+		// if (!(num % 2)) fn.classList.add("leftnote");
 
 		// put note flush with reference, but don't overlap previous notes
 		fn.style.top = (Math.max(prevbot, reftop)) + "px";
 
-		fnref.onmouseover = function(e) { fn.style.background = "#FAFAFA"; };
+		fnref.onmouseover = function(e) { fn.style.background = "#f6f5eb"; };
 		fnref.onmouseout  = function(e) { fn.style.background = "none";    };
 	});
 }
