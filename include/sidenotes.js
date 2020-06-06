9ae2aee89f5389e5049ccf26a3b9110783d6f75f
Alex Klapheke <alexklapheke@gmail.com>
2020-06-06 15:01:30 -0400

    Fix image layouts; put captions in sidebar with sidenotes

--- include/sidenotes.js
+++ include/sidenotes.js
@@ -1,25 +1,38 @@
 window.onload = function() {
-	var fns = document.getElementsByClassName("footnotes")[0];
-	fns.classList.add("sidenotes");
-	document.querySelectorAll('.footnotes > ol > li').forEach(function(fn) {
-		var bodytop = document.body.getBoundingClientRect().top;
+	// Apply class to whole container
+	var footnotes = document.getElementsByClassName("footnotes");
+	for (let elem of footnotes){elem.classList.add("sidenotes")};
 
-		// get height of note reference
-		var fnref = document.getElementById(fn.id.replace(/fn/, 'fnref'));
+	var prevbot = 0
+	var bodytop = document.body.getBoundingClientRect().top;
+	document.querySelectorAll('.footnote-ref, figcaption').forEach(function(elem) {
+
+		// We do it this way because we have to do figures and footnotes in the order
+		// they appear in the text---if we just went through the footnotes list at the
+		// end of the file, we wouldn't know the relative order of footnotes and captions.
+		if (elem.tagName == "FIGCAPTION") {
+			fnref = elem.parentNode // the <figure> or whatever containing the <figcaption>
+			fn = elem // the <figcaption> itself
+		} else {
+			fnref = elem // the <a class="footnote-ref"> found in the text
+			fn = document.getElementById(elem.id.replace(/ref/, '')); // the <li> with corresponding id
+		}
+
+		// get position of footnote reference
 		var reftop = fnref.getBoundingClientRect().top - bodytop;
-
-		// get height of last note
-		var num = parseInt(fn.id.replace(/fn/, ''));
-		var prev = document.getElementById("fn" + (num-1));
-		var prevbot = (num < 2) ? 0 : prev.getBoundingClientRect().bottom - bodytop;
-
-		// // put even notes on left
-		// if (!(num % 2)) fn.classList.add("leftnote");
 
 		// put note flush with reference, but don't overlap previous notes
 		fn.style.top = (Math.max(prevbot, reftop)) + "px";
 
+		// so we can highlight notes when the mouse is over the note reference
 		fnref.onmouseover = function(e) { fn.classList.add("hovered"); };
 		fnref.onmouseout  = function(e) { fn.classList.remove("hovered"); };
+
+		if (fn.tagName == "FIGCAPTION") {
+			fn.classList.add("sidenote")
+		}
+
+		// store bottom of this note for positioning next note
+		prevbot = fn.getBoundingClientRect().bottom - bodytop
 	});
 }
