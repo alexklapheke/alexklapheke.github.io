27e0202ea8632695a830f4fbfa494134dac20269
Alex Klapheke <alexklapheke@gmail.com>
2020-06-11 17:55:01 -0400

    Add sortable tables, resume, portfolio links

--- include/sidenotes.js
+++ include/sidenotes.js
@@ -1,16 +1,18 @@
-window.onload = function() {
+window.addEventListener("load", function() {
 	// Apply class to whole container
 	var footnotes = document.getElementsByClassName("footnotes");
 	for (let elem of footnotes){elem.classList.add("sidenotes")};
 
 	var prevbot = 0
 	var bodytop = document.body.getBoundingClientRect().top;
-	document.querySelectorAll('.footnote-ref, figcaption').forEach(function(elem) {
+	document.querySelectorAll('.footnote-ref, figcaption, caption').forEach(function(elem) {
+
+		is_caption = (elem.tagName == "FIGCAPTION") || (elem.tagName == "CAPTION")
 
 		// We do it this way because we have to do figures and footnotes in the order
 		// they appear in the text---if we just went through the footnotes list at the
 		// end of the file, we wouldn't know the relative order of footnotes and captions.
-		if (elem.tagName == "FIGCAPTION") {
+		if (is_caption) {
 			fnref = elem.parentNode // the <figure> or whatever containing the <figcaption>
 			fn = elem // the <figcaption> itself
 		} else {
@@ -22,7 +24,7 @@ window.onload = function() {
 		var reftop = fnref.getBoundingClientRect().top - bodytop;
 
 		// I don't know why this adjustment is necessary
-		if (elem.tagName == "FIGCAPTION") {
+		if (is_caption) {
 			reftop += 14;
 		}
 
@@ -33,11 +35,11 @@ window.onload = function() {
 		fnref.onmouseover = function(e) { fn.classList.add("hovered"); };
 		fnref.onmouseout  = function(e) { fn.classList.remove("hovered"); };
 
-		if (fn.tagName == "FIGCAPTION") {
+		if (is_caption) {
 			fn.classList.add("sidenote")
 		}
 
 		// store bottom of this note for positioning next note
 		prevbot = fn.getBoundingClientRect().bottom - bodytop
 	});
-}
+});
