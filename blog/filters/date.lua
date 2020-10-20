8ae410ce07fc84dcc8b75838b0c7f58b74a6561d
Alex Klapheke <alexklapheke@gmail.com>
2020-10-20 17:18:23 -0400

    Fix dates in RSS feed to be RFC822

--- blog/filters/date.lua
+++ blog/filters/date.lua
@@ -1,6 +1,7 @@
 function Meta(m)
     date = m.date
-    m.date = os.date("%Y-%m-%dT%H:%M:%S%z", date)  -- 2020-06-22T06:10:59-0400
+    m.date_iso = os.date("%Y-%m-%dT%H:%M:%S%z", date)  -- 2020-06-22T06:10:59-0400
+    m.date_rfc = os.date("%a, %d %b %Y %T %z", date)  -- Mon, 22 Jun 2020 06:10:59 -0400
     m.date_pretty = os.date("%A, %B %e, %Y", date) -- Monday, June 22, 2020
     m.date_terse = os.date("%F", date)             -- 2020-06-22
     return m
